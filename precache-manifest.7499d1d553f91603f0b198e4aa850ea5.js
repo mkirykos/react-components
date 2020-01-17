7a40ccee0adaffbb4195fdfbe6c2cc82dcd57fd0
Zopa Ltd <frontend-opensource@zopa.com>
2020-01-17 11:33:03 +0000

    [ci skip] - circleci

---
+++ precache-manifest.7499d1d553f91603f0b198e4aa850ea5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df1c06df7957235389a",
+    "url": "build/2.1df1c06d.js"
+  },
+  {
+    "revision": "a9beb19e3bd39ee7bd04fa8523d2713a",
+    "url": "build/2.1df1c06d.js.LICENSE"
+  },
+  {
+    "revision": "47e562ce73a2b423b95d",
+    "url": "build/bundle.47e562ce.js"
+  },
+  {
+    "revision": "db5aa53f02d21a4a8473",
+    "url": "build/main.db5aa53f.js"
+  },
+  {
+    "revision": "e60b5f4b5b7861d2fe0c18e6080c4b60",
+    "url": "index.html"
+  },
+  {
+    "revision": "1df1c06df7957235389a",
+    "url": "static/css/2.68d3bff2.chunk.css"
+  },
+  {
+    "revision": "7670e9211931ecfc1626e7b8cb4dd393",
+    "url": "static/media/alert-icon.7670e921.svg"
+  },
+  {
+    "revision": "a16d4c678b946f4dfcc2ae0affaba512",
+    "url": "static/media/chevron-down.a16d4c67.svg"
+  },
+  {
+    "revision": "1cb043bc63b8bd0e81d73fcdac40c90e",
+    "url": "static/media/red-warning.1cb043bc.svg"
+  },
+  {
+    "revision": "6539411e25e614db294c638fbbc3612d",
+    "url": "static/media/triumph-icon.6539411e.svg"
+  },
+  {
+    "revision": "4593b2739f118a6e3dee694073ba8a80",
+    "url": "static/media/valid-icon.4593b273.svg"
+  },
+  {
+    "revision": "1c85fd4b04f72c51c0f63f545aee7707",
+    "url": "static/media/white-check-mark.1c85fd4b.svg"
+  },
+  {
+    "revision": "8c2a94ba93ab4b82512d1b2d8c7c96f3",
+    "url": "static/media/zopaLogo.8c2a94ba.svg"
+  }
+]);
